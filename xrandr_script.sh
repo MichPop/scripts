5127cd5fe916ac1ffc83f4f2315e13ef0747e7e2
mipopka <popovicmichaelm@gmail.com>
2020-12-29 15:56:57 +0100

    scripts

--- xrandr_script.sh
+++ xrandr_script.sh
@@ -1,6 +1,5 @@
 if [ $(xrandr|grep -c 'HDMI1 connected') = '1' ]; then
 	xrandr --output HDMI1 --auto --primary --left-of eDP1 
-	xrandr --output DP1 --auto --left-of HDMI1 --rotate right
 	xrandr --output eDP1 --off
 	xwallpaper --output HDMI1 --stretch ~/.config/wallpaper.jpg
 else
